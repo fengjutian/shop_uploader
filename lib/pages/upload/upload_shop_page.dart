297d66dd109d5971b74f5d17dc3dfab4b3b1c844
fengjutian <fjutian@foxmail.com>
2026-01-29 20:53:52 +0800

    refactor(upload): 简化店铺上传页面的表单实现
    移除冗余的TDForm组件及相关状态管理，改用更简洁的TextFormField和ElevatedButton

--- lib/pages/upload/upload_shop_page.dart
+++ lib/pages/upload/upload_shop_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tdesign_flutter/tdesign_flutter.dart';
-import '../../models/shop_model.dart';
 import '../../services/shop_api.dart';
 import '../../widgets/image_grid.dart';
 
@@ -12,14 +11,9 @@ class UploadShopPage extends StatefulWidget {
 }
 
 class _UploadShopPageState extends State<UploadShopPage> {
-  final _formController = TDFormController();
   final _formKey = GlobalKey<FormState>();
   final nameCtrl = TextEditingController();
   final addrCtrl = TextEditingController();
-  final Map<String, TDFormItemNotifier> _formItemNotifier = {
-    'name': TDFormItemNotifier(),
-    'address': TDFormItemNotifier(),
-  };
 
   List<PickedImage> images = [];
 
@@ -36,7 +30,6 @@ class _UploadShopPageState extends State<UploadShopPage> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = TDTheme.of(context);
     return Scaffold(
       appBar: AppBar(title: const Text('上传店铺')),
       body: Padding(
@@ -45,69 +38,22 @@ class _UploadShopPageState extends State<UploadShopPage> {
           key: _formKey,
           child: ListView(
             children: [
-              TDForm(
-                formController: _formController,
-                formContentAlign: TextAlign.left,
-                requiredMark: true,
-                formShowErrorMessage: true,
-                items: [
-                  TDFormItem(
-                    label: '店铺名称',
-                    name: 'name',
-                    type: TDFormItemType.input,
-                    help: '请输入店铺名称',
-                    labelWidth: 82.0,
-                    formItemNotifier: _formItemNotifier['name'],
-                    showErrorMessage: true,
-                    requiredMark: true,
-                    child: TDInput(
-                      leftContentSpace: 0,
-                      inputDecoration: InputDecoration(
-                        hintText: '请输入店铺名称',
-                        border: InputBorder.none,
-                        hintStyle: TextStyle(color: theme.textColorPlaceholder),
-                      ),
-                      controller: nameCtrl,
-                      additionInfoColor: theme.errorColor6,
-                      showBottomDivider: false,
-                      onChanged: (val) {
-                        _formItemNotifier['name']?.upDataForm(val);
-                      },
-                      onClearTap: () {
-                        nameCtrl.clear();
-                        _formItemNotifier['name']?.upDataForm('');
-                      },
-                    ),
-                  ),
-                  TDFormItem(
-                    label: '店铺地址',
-                    name: 'address',
-                    type: TDFormItemType.input,
-                    help: '请输入店铺地址',
-                    labelWidth: 82.0,
-                    formItemNotifier: _formItemNotifier['address'],
-                    showErrorMessage: true,
-                    requiredMark: true,
-                    child: TDInput(
-                      leftContentSpace: 0,
-                      inputDecoration: InputDecoration(
-                        hintText: '请输入店铺地址',
-                        border: InputBorder.none,
-                        hintStyle: TextStyle(color: theme.textColorPlaceholder),
-                      ),
-                      controller: addrCtrl,
-                      additionInfoColor: theme.errorColor6,
-                      showBottomDivider: false,
-                      onChanged: (val) {
-                        _formItemNotifier['address']?.upDataForm(val);
-                      },
-                      onClearTap: () {
-                        addrCtrl.clear();
-                        _formItemNotifier['address']?.upDataForm('');
-                      },
-                    ),
-                  ),
-                ],
+              TextFormField(
+                controller: nameCtrl,
+                decoration: const InputDecoration(
+                  labelText: '店铺名称',
+                  hintText: '请输入店铺名称',
+                ),
+                validator: (value) => value?.isEmpty ?? true ? '请输入店铺名称' : null,
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: addrCtrl,
+                decoration: const InputDecoration(
+                  labelText: '店铺地址',
+                  hintText: '请输入店铺地址',
+                ),
+                validator: (value) => value?.isEmpty ?? true ? '请输入店铺地址' : null,
               ),
               const SizedBox(height: 16),
               ImageGrid(
@@ -117,13 +63,12 @@ class _UploadShopPageState extends State<UploadShopPage> {
                     setState(() => images.removeWhere((img) => img.id == id)),
               ),
               const SizedBox(height: 16),
-              TDButton(
-                type: TDButtonType.fill,
-                theme: TDButtonTheme.primary,
-                size: TDButtonSize.large,
-                isBlock: true,
-                text: '提交',
-                onTap: submit,
+              ElevatedButton(
+                onPressed: submit,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text('提交'),
               ),
             ],
           ),
